b71f596aacb5df853816724c4a61d0189cd2c75f
Nabzozifo <nabzozifo@gmail.com>
2021-10-06 14:21:35 +0000

    sendmail2

--- forms/contact.php
+++ forms/contact.php
@@ -39,7 +39,7 @@
 
   echo $contact->send();*/
 
-  <?php 
+ 
 if(isset($_POST['submit'])){
     $to = "endiayemouhamednaby96o@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
@@ -57,5 +57,5 @@ if(isset($_POST['submit'])){
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
-?>
+
 ?>
